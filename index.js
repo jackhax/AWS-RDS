cefa56d681b23e385393bd033e6eaad66cb8c21e
Adnan Jakati <01fe18bec080@kletech.ac.in>
2021-10-21 19:06:05 +0530

    big fix

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ nam = document.getElementById('nam')
 cgpa = document.getElementById('cgpa')
 radios = document.getElementsByName('cgpa');
 query = ""
+
+function urlencode(str) {
+  str = (str + '').toString();
+
+  // Tilde should be allowed unescaped in future versions of PHP (as reflected below), but if you want to reflect current
+  // PHP behavior, you would need to add ".replace(/~/g, '%7E');" to the following.
+  return encodeURIComponent(str)
+    .replace('!', '%21')
+    .replace('\'', '%27')
+    .replace('(', '%28')
+    .replace(')', '%29')
+    .replace('*', '%2A')
+    .replace('%20', '+');
+}
+
 function submit_form(){
 	u = usn.value
 	n = nam.value
@@ -15,7 +30,7 @@ function submit_form(){
 	        }
 	    }
 
-	query2 = `select * from students_data where name like "%${n}%" and usn like "%25${u}%"`
+	query2 = `select * from students_data where name like "%25${n}%" and usn like "%25${u}%"`
 
 	if(c !== ''){
 		query2 += ` and cgpa ${type} ${c}`
